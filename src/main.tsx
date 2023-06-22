3564885c808dc1df5721e2be6f97b72896f266f8
sergiohdljr <sergio.tjf1@gmail.com>
2023-06-21 21:08:01 -0300

    fix: import on index

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { GlobalStyle } from "./styles/global";
-import { Home } from "./pages/home";
+import { Home } from "./pages";
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
