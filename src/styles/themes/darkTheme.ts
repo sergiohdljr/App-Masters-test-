eaa1ddbd8ef6303bfe006be63ef01cc8b818c0cb
sergiohdljr <sergio.tjf1@gmail.com>
2023-06-23 01:51:51 -0300

    feat: add themes

---
+++ src/styles/themes/darkTheme.ts
@@ -0,0 +1,9 @@
+export const darkTheme = {
+  background: "#141D2F",
+  contentBackground: "#1E2A47",
+  text: "white",
+  textAlt: "white",
+  shadow: "0px 16px 30px -10px rgba(70, 96, 187, 0.2);",
+  btn: "#0079ff",
+  btnHover: "#60abff",
+};
