1f0225f03de97e34ad86d4642279ba2b0f68b303
sergiohdljr <sergio.tjf1@gmail.com>
2023-06-25 11:23:37 -0300

    fix build

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { GlobalStyle } from "./styles/global";
 import { Home } from "./pages";
 import { QueryClientProvider } from "react-query";
 import { queryCLient } from "./service/client";
-import { ThemeProvider } from "styled-components";
-import { lightTheme } from "./styles/themes";
-import { theme } from "./store/themeStore";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryCLient}>
-        <Home />
+      <Home />
     </QueryClientProvider>
   </React.StrictMode>
 );
